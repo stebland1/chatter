5c6d323685538f099a93257311ded00b2546859b
Stephen Bland <stebland1@gmail.com>
2025-07-30 12:54:13 +0100

    refactor(server): seperate handling funcs for cnxns and msgs

--- include/server.h
+++ include/server.h
@@ -1,13 +1,24 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <sys/socket.h>
 #include <sys/types.h>
 
 #define MAX_MSG_LEN 512
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
-void send_message(int listenerfd, char *msg, int from, int maxfd, fd_set *fds);
-void close_all_fds(int maxfds, fd_set *fds);
+typedef struct {
+  fd_set masterfds;
+  fd_set readfds;
+  int maxfd;
+  int listenerfd;
+} ServerContext;
+
+void send_message(int senderfd, char *msg, ServerContext *ctx);
+void close_all_fds(ServerContext *ctx);
 int get_listener_socket(const char *hostname, const char *port);
+int handle_client_message(int senderfd, ServerContext *ctx);
+void handle_new_connection(ServerContext *ctx, struct sockaddr *clientaddr,
+                           socklen_t *clientaddr_size);
 
 #endif
--- src/server.c
+++ src/server.c
@@ -8,9 +8,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-void close_all_fds(int maxfds, fd_set *fds) {
-  for (int i = 0; i <= maxfds; i++) {
-    if (FD_ISSET(i, fds)) {
+void close_all_fds(ServerContext *ctx) {
+  for (int i = 0; i <= ctx->maxfd; i++) {
+    if (FD_ISSET(i, &ctx->masterfds)) {
       close(i);
     }
   }
@@ -72,11 +72,55 @@ int get_listener_socket(const char *hostname, const char *port) {
   return -1;
 }
 
-void send_message(int listenerfd, char *msg, int from, int maxfd, fd_set *fds) {
+int handle_client_message(int senderfd, ServerContext *ctx) {
+  char buf[MAX_MSG_LEN];
+  int bytes_received = recv(senderfd, buf, MAX_MSG_LEN, 0);
+  if (bytes_received <= 0) {
+    if (bytes_received == 0) {
+      // TODO: Relay user X left the chat.
+      printf("user-%d left the chat.\n", senderfd);
+    }
+
+    close(senderfd);
+    // TODO: Every time we remove a fd, check to see if it's the
+    // largest.
+    FD_CLR(senderfd, &ctx->masterfds);
+    return 0;
+  }
+
+  buf[bytes_received] = '\0';
+  char *msg = malloc(strlen(buf) + 1 /* null */ + 7 + get_num_digits(senderfd));
+  if (!msg) {
+    fprintf(stderr, "Failed to allocate memory for message\n");
+    return -1;
+  }
+  sprintf(msg, "user-%d: %s", senderfd, buf);
+  send_message(senderfd, msg, ctx);
+  free(msg);
+  return 0;
+}
+
+void handle_new_connection(ServerContext *ctx, struct sockaddr *clientaddr,
+                           socklen_t *clientaddr_size) {
+  int connectionfd = accept(ctx->listenerfd, clientaddr, clientaddr_size);
+  if (connectionfd < 0) {
+    return;
+  }
+
+  // TODO: Relay user X entered the chat.
+  printf("user-%d entered the chat.\n", connectionfd);
+
+  FD_SET(connectionfd, &ctx->masterfds);
+  if (connectionfd > ctx->maxfd) {
+    ctx->maxfd = connectionfd;
+  }
+}
+
+void send_message(int senderfd, char *msg, ServerContext *ctx) {
   // TODO: Apparently send buffers messages
   // So we could end up with bytes still to send.
-  for (int i = 0; i <= maxfd; i++) {
-    if (FD_ISSET(i, fds) && i != from && i != listenerfd) {
+  for (int i = 0; i <= ctx->maxfd; i++) {
+    if (FD_ISSET(i, &ctx->masterfds) && i != senderfd && i != ctx->listenerfd) {
       int bytes_sent = send(i, msg, strlen(msg), 0);
       if (bytes_sent == -1) {
         perror("send");
@@ -106,74 +150,44 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
 
-  fd_set masterfds, readfds;
-  int maxfd = listenerfd;
-  FD_ZERO(&masterfds);
-  FD_ZERO(&readfds);
-  FD_SET(listenerfd, &masterfds);
+  ServerContext ctx = {
+      .masterfds = {0},
+      .readfds = {0},
+      .listenerfd = listenerfd,
+      .maxfd = listenerfd,
+  };
+
+  FD_ZERO(&ctx.masterfds);
+  FD_ZERO(&ctx.readfds);
+  FD_SET(listenerfd, &ctx.masterfds);
 
   while (1) {
-    readfds = masterfds;
+    ctx.readfds = ctx.masterfds;
     struct sockaddr_storage clientaddr;
     socklen_t clientaddr_size = sizeof clientaddr;
 
-    if (select(maxfd + 1, &readfds, NULL, NULL, NULL) < 0) {
+    if (select(ctx.maxfd + 1, &ctx.readfds, NULL, NULL, NULL) < 0) {
       if (errno == EINTR) {
         continue;
       } else {
-        close_all_fds(maxfd, &masterfds);
+        close_all_fds(&ctx);
         fprintf(stderr, "Error selecting ready file desriptors\n");
         return EXIT_FAILURE;
       }
     }
 
-    for (int i = 0; i <= maxfd; i++) {
-      if (FD_ISSET(i, &readfds)) {
+    for (int i = 0; i <= ctx.maxfd; i++) {
+      if (FD_ISSET(i, &ctx.readfds)) {
         if (i == listenerfd) {
-          int connectionfd = accept(listenerfd, (struct sockaddr *)&clientaddr,
-                                    &clientaddr_size);
-          if (connectionfd < 0) {
-            continue;
-          }
-
-          // TODO: Relay user X entered the chat.
-          printf("user-%d entered the chat.\n", connectionfd);
-
-          FD_SET(connectionfd, &masterfds);
-          if (connectionfd > maxfd) {
-            maxfd = connectionfd;
-          }
-        } else {
-          char buf[MAX_MSG_LEN];
-          int bytes_received = recv(i, buf, MAX_MSG_LEN, 0);
-          if (bytes_received <= 0) {
-            if (bytes_received == 0) {
-              // TODO: Relay user X left the chat.
-              printf("user-%d left the chat.\n", i);
-            }
-
-            close(i);
-            // TODO: Every time we remove a fd, check to see if it's the
-            // largest.
-            FD_CLR(i, &masterfds);
-            continue;
-          }
-
-          buf[bytes_received] = '\0';
-          char *msg =
-              malloc(strlen(buf) + 1 /* null */ + 7 + get_num_digits(i));
-          if (!msg) {
-            fprintf(stderr, "Failed to allocate memory for message\n");
-            return EXIT_SUCCESS;
-          }
-          sprintf(msg, "user-%d: %s", i, buf);
-          send_message(listenerfd, msg, i, maxfd, &masterfds);
-          free(msg);
+          handle_new_connection(&ctx, (struct sockaddr *)&clientaddr,
+                                &clientaddr_size);
+        } else if (handle_client_message(i, &ctx) < 0) {
+          return EXIT_FAILURE;
         }
       }
     }
   }
 
-  close_all_fds(maxfd, &masterfds);
+  close_all_fds(&ctx);
   return EXIT_SUCCESS;
 }
