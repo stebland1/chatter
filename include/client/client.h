dfb31153f59b393a7c6442c481bf24655a7594f0
Stephen Bland <stebland1@gmail.com>
2025-09-02 11:37:28 +0100

    fix(client.h): remove redundant client context structure

--- include/client/client.h
+++ include/client/client.h
@@ -3,11 +3,6 @@
 
 #include <sys/socket.h>
 
-typedef struct {
-  fd_set readfds;
-  int maxfd;
-} ClientContext;
-
 int client_connect(const char *hostname, const char *port);
 int client_run(int serverfd);
 void client_shutdown(int serverfd);
