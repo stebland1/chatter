97a6db5d249c484d17a06262f6f3a858194e19ce
Stephen Bland <stebland1@gmail.com>
2025-09-02 11:03:15 +0100

    fix(server.h): remove redundant connected clients structure

--- include/server/server.h
+++ include/server/server.h
@@ -2,7 +2,6 @@
 #define SERVER_H
 
 #include "poll.h"
-#include "server/client.h"
 #include "server/hashtable.h"
 #include <stddef.h>
 #include <sys/socket.h>
@@ -13,12 +12,6 @@ typedef struct {
   HashTable *connected_clients;
   Poll *poll;
 } ServerContext;
-
-typedef struct {
-  Client *clients;
-  size_t count;
-  size_t capacity;
-} ConnectedClients;
 
 int server_init(ServerContext *ctx, const char *hostname, const char *port);
 void server_run(ServerContext *ctx);
